debedc19071aae72c7db43a31c8aa309df7c706c
Pedro Diaz <peterdiaz164@gmail.com>
2025-02-14 03:36:06 -0600

    fixed landing page

--- frontend/src/components/LandingPage.js
+++ frontend/src/components/LandingPage.js
@@ -13,12 +13,12 @@ function LandingPage() {
 
   return (
     <div className="landing-container">
-      <h1 className="landing-title">Welcome to the Nursing Hours Tracker</h1>
+      <h1 className="landing-title">Hello, Alexis!</h1>
       <div className="landing-tiles-container">
         {/* One tile that looks like "pretty-adaptation" in Railway */}
         <div className="landing-tile" onClick={handleTileClick}>
-          <h2>Nursing-Hours-Tracker</h2>
-          <p>Click to Open Project</p>
+          <h2>Nursing Hours Tracker</h2>
+          <p>Click to Open</p>
         </div>
       </div>
     </div>
